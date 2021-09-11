1747e999a41823dc5c7e3e8fe5ddaf15b51fb576
DongHwiCho <junejoe97@inha.edu>
2021-09-11 19:28:46 +0900

    ReviewDemo01.java Version 0.2

--- README.txt
+++ README.txt
@@ -2,3 +2,6 @@ ReviewDemo01.java History
 
 Version 0.1
  - 소수를 판별하는 프로그램 생성
+
+ Version 0.2
+ - for문 반복횟수 2회 감소
--- ReviewDemo01.java
+++ ReviewDemo01.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-// v0.1
+// v0.2
 public class ReviewDemo01 {
     public static void main(String[] args) {
         // 소수 : 1과 자기자신외에는 나누어 떨어지지 않는 수
@@ -7,13 +7,13 @@ public class ReviewDemo01 {
         int number = sc.nextInt();
         int cnt = 0;
 
-        for(int k=1; k<=number; k++) {
-            if(number % k == 0) {
+        for(int k=2; k<number; k++) {  // 반복 횟수 2회 감소
+            if(number % k == 0) {  // 입력된 수를 k로 나누었을 때 나머지가 0인 경우. 즉 약수
                 cnt++;
             }
         }
 
-        if(cnt == 2) {
+        if(cnt == 0) {
             System.out.println(number + "은(는) 소수!");
         }
         else {
